01d0ac683eb368aa7396cef67943e29a1fb94aba
James XPS <jimbotops@hotmail.co.uk>
2020-05-03 18:05:34 +0100

    initial commit

---
+++ precache-manifest.54cbdd7f8a2df07d9096682c9e673c38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc12036173068e80b99c5fbf440c4b77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35c2535145b1655c4d62",
+    "url": "/static/css/main.196f015c.chunk.css"
+  },
+  {
+    "revision": "5666c5f74d3016750476",
+    "url": "/static/js/2.7e6291aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7e6291aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35c2535145b1655c4d62",
+    "url": "/static/js/main.4d9141e4.chunk.js"
+  },
+  {
+    "revision": "be3081f0e9c87d05edf3",
+    "url": "/static/js/runtime-main.c45557fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
